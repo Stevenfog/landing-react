a167f95c7ffea7894d4d3a18add0af8793f1d058
Steven Hermadoni <stevenbutar51@gmail.com>
2023-06-03 09:10:39 +0700

    fix App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './assets/scss/bootstrap.scss'
-import LandingPage from './homepage/LandingPage';
-function App() {
-  return (
-    <div className="App">
-      <LandingPage />
-    </div>
-  );
+import React from 'react'
+import Footer from '../component/Footer'
+import Headers from '../component/Header'
+import Hero from '../component/Hero'
+import Special from '../component/Special'
+import landingPageJson from '../json/landingPage.json'
+
+export default function LandingPage() {
+    return (
+        <>
+            <Headers />
+            <Hero />
+            <Special data={landingPageJson.special} />
+            <Footer />
+        </>
+    )
 }
-export default App;
